20ee49ea5dc794c04f05dd9ca934459b5674e9be
Rud Demons <rudah.tankka@gmail.com>
2020-11-09 22:13:27 -0300

    brincando um pouco
    o bot te responde com ou sem @, só depende do que você irá dizer.

--- bot.js
+++ bot.js
@@ -4,3 +4,24 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config();
 client.login(process.env.RUDBOT_TOKEN);
+
+client.on('ready', () => {
+    
+    console.log("tá logado pai")
+})
+
+client.on('message', (msg) => {
+
+    let canalBot = '775522793462825021'
+
+    console.log(msg.content);
+    if (msg.channel.id == canalBot && msg.content === 'o que você faz Rudbot?') {
+        
+        msg.reply('eu fumo makonha')
+    }
+
+    if (msg.channel.id == canalBot && msg.content === 'mas makonha não existem') {
+        
+        msg.channel.send('FUMEI TUDO PORRA')
+    }
+})
